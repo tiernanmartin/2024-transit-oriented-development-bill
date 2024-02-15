d6e7e6c226584a5ebea39ac3c32b051a51878db2
Tiernan Martin <tiernan.martin@gmail.com>
2024-02-15 10:16:42 -0800

    refactor target pipeline

--- _targets.R
+++ _targets.R
@@ -13,19 +13,42 @@ library(here)
 
 # SET TARGET OPTIONS ----
 tar_option_set(
-  packages = c("tibble","tidyverse","DBI","RPostgres","glue")
+  packages = c("tibble",
+               "tidyverse",
+               "DBI",
+               "RPostgres",
+               "glue",
+               "sf",
+               "janitor",
+               "mapview",
+               "readxl",
+               "here")
 )
 
 # SOURCE R FUNCTIONS ----
 
 tar_source(files = here("R/functions.R"))
 
-# DEFINE TARGET PLAN ----
+# DEFINE TARGET PIPELINE ----
 
-list(
-  tar_target(landuse_codes_file, 
-             here("data/Washington State Standard Assessment 2-Digit Codes.xlsx"), 
-             format = "file"),
-  tar_target(landuse_codes, 
-             load_landuse_codes(landuse_codes_file))
-)
+pipeline_files <- 
+  list(
+    tar_target(landuse_codes_file, 
+               here("data/Washington State Standard Assessment 2-Digit Codes.xlsx"), 
+               format = "file")
+    
+  )
+
+pipeline_postgres <- 
+  list(
+    tar_target(landuse_codes, 
+               load_landuse_codes(landuse_codes_file))
+  )
+
+pipeline <- c(pipeline_files,
+              pipeline_postgres)
+
+
+# RUN TARGET PIPELINE -----------------------------------------------------
+
+pipeline
