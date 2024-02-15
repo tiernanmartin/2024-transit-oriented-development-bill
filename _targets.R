7227074a8e100be1e692134227fe01103dc749d1
Tiernan Martin <tiernan.martin@gmail.com>
2024-02-15 05:13:26 -0800

    define database connection parameters

--- _targets.R
+++ _targets.R
@@ -4,12 +4,20 @@ library(targets)
 
 # SET TARGET OPTIONS ----
 tar_option_set(
-  packages = c("tibble")
+  packages = c("tibble","tidyverse","DBI","RPostgres")
 )
 
 # SOURCE R FUNCTIONS ----
 
 tar_source(files = "R/functions.R") 
+
+# GET DATABASE CONNECTION PARAMETERS
+
+db_host <- Sys.getenv("DB_HOST")
+db_port <- Sys.getenv("DB_PORT")
+db_name <- Sys.getenv("DB_NAME")
+db_user <- Sys.getenv("DB_USER")
+db_password <- Sys.getenv("POSTGRES_TOD_PASSWORD")
 
 # DEFINE TARGET PLAN ----
 
